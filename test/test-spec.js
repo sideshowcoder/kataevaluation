bc4dccd68d6aa5527cab8e873c7eae7dc65153e2
Philipp Fehre <philipp.fehre@googlemail.com>
2014-02-08 11:23:55 +0100

    test 2

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,7 +1,21 @@
+function Cell(){
+	
+}
+
+Cell.prototype.alive = function(){
+	return false;
+} 
 
 describe('Game of Life', function() {
   it('a new cell is dead by default', function(){
 	var cell = new Cell()
 	expect(cell.alive()).toBe(false)
+  });
+
+  it('live cell with less than two live neighbors dies', function(){
+	var cell = new Cell();
+	cell.neighbours = 1;
+	cell.applyRules();
+	expect(cell.alive()).toBe(false);
   });
 });
