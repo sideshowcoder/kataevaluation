5e27e6896257753f96644a50ee7bd2a6530df28d
Philipp Fehre <philipp.fehre@googlemail.com>
2014-02-08 11:39:22 +0100

    test

--- test/test-spec.js
+++ test/test-spec.js
@@ -7,26 +7,25 @@ Cell.prototype.alive = function(){
 } 
 
 Cell.prototype.applyRules = function(){
-	if(this.neighbours > 3) {
-		this._alive = false
-	}
-	
 	if(this.neighbours === 3) {
 		this._alive = true
 	}
-	if(this.neighbours < 2) {
+	if(this.neighbours < 2 || this.neighbours > 3) {
 		this._alive = false;
 	}
 }
 
 describe('Game of Life', function() {
+  var cell
+  beforeEach(function(){
+    cell = new Cell()
+  })
+  
   it('a new cell is dead by default', function(){
-	  var cell = new Cell()
 	  expect(cell.alive()).toBe(false)
   });
 
   it('live cell with less than two live neighbors dies', function(){
-	  var cell = new Cell();
 	  cell._alive = true;
 	  cell.neighbours = 1;
 	  cell.applyRules();
@@ -34,7 +33,6 @@ describe('Game of Life', function() {
   });
   
   it("a living cell with more than 3 neighbours dies", function(){
-    var cell = new Cell();
 	  cell._alive = true;
 	  cell.neighbours = 4;
 	  cell.applyRules();
@@ -42,7 +40,6 @@ describe('Game of Life', function() {
   })
 	
   it('dead cell with exactly three live neighbors comes alive', function(){
-	  var cell = new Cell();
 	  cell.neighbours = 3;
 	  cell.applyRules();
 		expect(cell.alive()).toBe(true);
