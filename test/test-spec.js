f6133344bbedd40d702454bbcc6738c3ae786ecf
Philipp Fehre <philipp.fehre@googlemail.com>
2014-02-08 11:16:37 +0100

    test 1

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,7 @@
-describe('test', function() {
-  it('test', function(){
-    expect(true).toBe(true);
+
+describe('Game of Life', function() {
+  it('a new cell is dead by default', function(){
+	var cell = new Cell()
+	expect(cell.alive()).toBe(false)
   });
 });
